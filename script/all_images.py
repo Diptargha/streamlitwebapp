d08ca84c01650219fc76bbabb7fd54ac132ee35b
diptarghac <72461551+diptarghac@users.noreply.github.com>
2021-10-10 14:57:30 +0100

    new datacharts.json uploaded

--- script/all_images.py
+++ script/all_images.py
@@ -1,6 +1,7 @@
 
 image_sidebar = 'images/analysis.json'
-image_datacharts = 'images/datacharts.json'
+image_datacharts1 = 'images/datacharts1.json'
+image_datacharts2 = 'images/datacharts2.json'
 image_comingsoon = 'images/commingsoon.json'
 image_author = 'https://github.com/Diptargha/streamlitwebapp/blob/master/images/author.jpeg?raw=true'
 image_step1 = "https://github.com/Diptargha/streamlitwebapp/blob/master/images/step%201.PNG?raw=true"
